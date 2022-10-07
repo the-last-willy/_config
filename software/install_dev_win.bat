57fbc5bf2e9ae3d710f7d142e36240706d2b164d
Willy Jacquet <willy.jacquet@outlook.com>
2022-10-07 13:25:54 +0200

    Create install_dev_win.bat

---
+++ software/install_dev_win.bat
@@ -0,0 +1,7 @@
+winget install -e --id BaldurKarlsson.RenderDoc
+winget install -e --id Git.Git
+winget install -e --id JetBrains.CLion
+winget install -e --id Kitware.CMake
+winget install -e --id Microsoft.VisualStudio.2022.Community-Preview
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Python.Python.3.10
