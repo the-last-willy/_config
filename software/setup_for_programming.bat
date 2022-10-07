e3eed843f422a8d25daabced24ecef5d224650f7
Willy Jacquet <willy.jacquet@outlook.com>
2022-10-07 13:30:11 +0200

    Update and rename install_dev.bat to setup_for_programming.bat

--- software/setup_for_programming.bat
+++ software/setup_for_programming.bat
@@ -1,6 +1,9 @@
+.\setup_common.bat
+
 winget install -e --id BaldurKarlsson.RenderDoc
 winget install -e --id Git.Git
 winget install -e --id JetBrains.CLion
+winget install -e --id JFrog.Conan
 winget install -e --id Kitware.CMake
 winget install -e --id Microsoft.VisualStudio.2022.Community-Preview
 winget install -e --id Microsoft.VisualStudioCode
