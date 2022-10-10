60dd181b7ec0481f9b66cc7b5fa3494a09645738
Willy Jacquet <willy.jacquet@outlook.com>
2022-10-10 10:27:15 +0200

    Update setup_for_programming.bat

--- software/setup_for_programming.bat
+++ software/setup_for_programming.bat
@@ -7,6 +7,7 @@ winget install -e --id Git.Git
 winget install -e --id JetBrains.CLion
 winget install -e --id JFrog.Conan
 winget install -e --id Kitware.CMake
+winget install -e --id LLVM.LLVM
 winget install -e --id Microsoft.VisualStudio.2022.Community-Preview
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Python.Python.3.10
