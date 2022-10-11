92eeccf0967e2522f168c76baee0bed7a9d8f569
Willy Jacquet <willy.jacquet@outlook.com>
2022-10-11 19:28:49 +0200

    Update setup_common.bat

--- software/setup_common.bat
+++ software/setup_common.bat
@@ -2,3 +2,4 @@ winget install -e --id Discord.Discord
 winget install -e --id IrfanSkiljan.IrfanView
 winget install -e --id Mozilla.Firefox
 winget install -e --id Spotify.Spotify
+winget install -e --id voidtools.Everything
